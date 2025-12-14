e49ddadc8dd1d2f06cc54ec1c9dcf3b6e8895517
Hope Kalitera <hopekalitera@gmail.com>
2025-12-14 19:31:01 +0200

    chore: Export PaginatedReleasesResponse and GetReleasesParams types from release.service

--- frontend/app/features/releases/index.ts
+++ frontend/app/features/releases/index.ts
@@ -9,6 +9,7 @@ export { useReleases } from './hooks/useReleases';
 export { useRelease } from './hooks/useRelease';
 
 export { releaseService } from './services/release.service';
+export type { PaginatedReleasesResponse, GetReleasesParams } from './services/release.service';
 
 export type { Release, Track, CreateReleaseData, UpdateReleaseData, CreateTrackData } from './types';
 export { ReleaseStatus } from './types';
